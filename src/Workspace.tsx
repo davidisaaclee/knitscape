1b760e6bd6a5f901ff512ff880d88b15968b8bf2
David Lee <david.isaac.lee@gmail.com>
2022-05-11 10:02:00 -0700

    save

--- src/Workspace.tsx
+++ src/Workspace.tsx
@@ -1,28 +1,130 @@
+import { range } from "lodash";
+import * as React from "react";
 import { useAtom } from "jotai";
-import Row from "./Row";
+import Measure from "react-measure";
 import * as A from "./atoms";
 import styles from "./Workspace.module.scss";
-import { CSSForwardingProps, classNames } from "./utils";
+import { CSSForwardingProps } from "./utils";
+
+const stitchSize = 40;
+
+interface StitchDisplayInfo {
+  color: string;
+  isFocused: boolean;
+  column: number;
+}
 
 export function Workspace({ style, className }: CSSForwardingProps) {
+  const scrollRef = React.useRef<React.ElementRef<"div">>(null);
+  const [palette] = useAtom(A.palette);
   const [pattern] = useAtom(A.pattern);
   const [cursor] = useAtom(A.cursor);
 
+  const [bounds, setBounds] = React.useState<null | {
+    width: number;
+    height: number;
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  }>(null);
+
+  const rowsToDisplay = React.useMemo<
+    Array<{
+      stitches: StitchDisplayInfo[];
+      isBackgroundRow: boolean;
+      rowIndex: number;
+    }>
+  >(
+    () =>
+      range(2)
+        .map((rowOffset) => cursor.row - rowOffset)
+        .filter((rowIndex) => pattern[rowIndex] != null)
+        .map((rowIndex) => ({
+          rowIndex,
+          isBackgroundRow: rowIndex !== cursor.row,
+          stitches: pattern[rowIndex].map((s, column) => ({
+            color: palette[s],
+            isFocused: cursor.row === rowIndex && cursor.column === column,
+            column,
+          })),
+        })),
+    [pattern, palette, cursor]
+  );
+
+  React.useEffect(() => {
+    const scroller = scrollRef.current;
+    if (scroller == null) {
+      return;
+    }
+    const x = scroller.querySelector(`[data-stitchcolumn="${cursor.column}"]`);
+    if (x == null) {
+      return;
+    }
+    const rect = x.getBoundingClientRect();
+    const marginLeft = bounds == null ? 0 : bounds.width / 2;
+    scroller.scrollBy({ left: rect.left - marginLeft, behavior: "smooth" });
+  }, [cursor, scrollRef]);
+
   return (
-    <div className={classNames(styles.container, className)} style={style}>
-      <div className={styles.scrollContent}>
-        {[0, 1].map(
-          (offset) =>
-            pattern[cursor.row - offset] != null && (
-              <Row
-                key={offset}
-                className={styles.row}
-                rowIndex={cursor.row - offset}
-              />
-            )
-        )}
-      </div>
-    </div>
+    <Measure
+      bounds
+      onResize={(r) => {
+        setBounds(r.bounds!);
+      }}
+    >
+      {({ measureRef }) => (
+        <div ref={measureRef} className={className} style={style}>
+          <div ref={scrollRef} className={styles.scrollContainer}>
+            <div className={styles.scrollContent}>
+              {rowsToDisplay.map(
+                (row) =>
+                  pattern[row.rowIndex] != null && (
+                    <div
+                      key={row.rowIndex}
+                      className={styles.rowContainer}
+                      style={{
+                        paddingLeft: bounds == null ? 0 : bounds.width / 2,
+                        paddingRight: bounds == null ? 0 : bounds.width / 2,
+                      }}
+                    >
+                      {row.stitches.map((stitch) => (
+                        <div
+                          key={stitch.column}
+                          className={styles.stitch}
+                          data-stitchColumn={stitch.column}
+                          style={{
+                            backgroundColor: stitch.color,
+                            width: stitchSize,
+                            height: stitchSize,
+                            ...(stitch.isFocused
+                              ? {
+                                  transform: "scale(1.3)",
+                                  zIndex: 2,
+                                }
+                              : {}),
+
+                            ...(row.isBackgroundRow
+                              ? { transform: "scale(0.7)" }
+                              : {}),
+                          }}
+                        >
+                          {(stitch.column % 5 === 0 ||
+                            stitch.column === cursor.column) && (
+                            <label className={styles.columnIndex}>
+                              {stitch.column}
+                            </label>
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  )
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </Measure>
   );
 }
 
