e88e170a359cb3ba10b99946a6ed1519986763b0
David Lee <david.isaac.lee@gmail.com>
2022-05-19 09:17:35 -0700

    Add next stitch preview, count until next row to infobox

--- src/Infobox.tsx
+++ src/Infobox.tsx
@@ -5,17 +5,57 @@ import * as M from "./model";
 import { CSSForwardingProps, classNames } from "./utils";
 import styles from "./Infobox.module.scss";
 
+function pluralize(singular: string, plural: string): (n: number) => string {
+  return (n) => (n === 1 ? singular : plural);
+}
+
 export function Infobox({ className, style }: CSSForwardingProps) {
   const [cursor] = useAtom(A.cursor);
   const [pattern] = useAtom(A.pattern);
+  const [palette] = useAtom(A.palette);
   const nextStitchChange = React.useMemo(
     () => M.Pattern.countUntilStitchChange(pattern, cursor),
     [pattern, cursor]
   );
+  const stitchesUntilEndOfRow = React.useMemo(
+    () =>
+      cursor.directionHorizontal === "ltr"
+        ? M.Pattern.extents(pattern).width - cursor.column - 1
+        : cursor.column,
+    [pattern, cursor]
+  );
+  const nextStitchColor = React.useMemo(
+    () =>
+      nextStitchChange.stitch == null
+        ? null
+        : palette[nextStitchChange.stitch.colorIndex],
+    [palette, nextStitchChange.stitch]
+  );
 
   return (
     <div className={classNames(styles.container, className)} style={style}>
-      {nextStitchChange.count} stitches until next color
+      <div>
+        {nextStitchChange.count - 1} more{" "}
+        {pluralize("stitch", "stitches")(nextStitchChange.count - 1)} until next
+        color{" "}
+        {nextStitchColor != null && (
+          <span>
+            (
+            <span
+              className={styles.nextStitchColorPreview}
+              style={{
+                backgroundColor: nextStitchColor,
+              }}
+            />
+            )
+          </span>
+        )}
+      </div>
+      <div>
+        {stitchesUntilEndOfRow} more{" "}
+        {pluralize("stitch", "stitches")(stitchesUntilEndOfRow)} until end of
+        row
+      </div>
     </div>
   );
 }
